9dc57ec5bf58a7e2c6ab51dd1d37e179396ba9b7
liangjingtao <xiezhahe@gmail.com>
2020-01-01 17:43:55 +0800

    delete some

--- scripts/sensor_data_listener_bug.py
+++ scripts/sensor_data_listener_bug.py
@@ -23,22 +23,21 @@ pos_y = 0
 tgt_x = 10
 tgt_y = 10
 
-
 yaw = 0
-
 dist = 0
-dis_threshold = 0.5
 
 MODE = 'follow the line'
 Ranges = [0] * 720
 Obstacle_TH = 1.8
 angle_increment = 0
 
+
+wall_distance = 0
 d_left = 0
 d_right = 0
 d_front = 0
 
-wall_distance = 0
+
 def Clip(x, l, r):
 	return max(min(x, r), l)
 
@@ -50,9 +49,9 @@ def LaserScanProcess(data):
 	global d_right
 	global d_front
 	
-	d_front = min(Ranges[300:420])
-	d_left = min(Ranges[525:555])
-	d_right = min(Ranges[165:195])
+	d_front = min(Ranges[660:720] + Ranges[0:60])
+	d_left = min(Ranges[0:200])
+	d_right = min(Ranges[520:720])
 	
 	
 	Ranges = data.ranges
@@ -100,10 +99,9 @@ def get_dist(x, y, X, Y):
 	temp_dist = math.sqrt(temp_dist)
 	return temp_dist
 
-
 def Close(x1, y1, x2, y2):
 	temp_dist = get_dist(x1, y1, x2, y2)
-	return (temp_dist < dis_threshold)
+	return (temp_dist < 0.1)
 
 def Odometry_process(data):
 	global pos_x
@@ -133,22 +131,20 @@ def Follow_the_line():
 	global Ranges
 	global MODE
 	global d_front
-	global angle_increment
 	global pos_x
 	global pos_y
 	global tgt_x
 	global tgt_y
 	
+	
 	theta = math.atan2(tgt_y - pos_y , tgt_x - pos_x)
 	if abs(yaw - theta) < 1e-1:
 		if d_front < Obstacle_TH:
 			print('close!!')
-			LINX = -0.5
-			angz = 0
 			MODE = 'follow the wall step one'
 			return
 		else:
-			LINX = Clip((d_front - Obstacle_TH) * 1.2, 0.8, 1.6)
+			LINX = Clip((d_front - Obstacle_TH) * 1.2, 0.5, 1.6)
 			angz = 0
 			return
 	
@@ -160,7 +156,7 @@ def Follow_the_line():
 	elif delta < -PI:
 		delta = 2 * PI + delta
 	
-	LINX = Clip((d_front - Obstacle_TH) * 1.2, 0.8, 1.6) + (-abs(delta)) * 0.7
+	LINX = Clip((d_front - Obstacle_TH) * 1.2, 0.5, 1.6) + (-abs(delta)) * 0.9
 	angz = delta * 2
 	return
 		
@@ -179,8 +175,8 @@ def Follow_the_wall():
 	global tgt_y
 	global wall_distance
 	if MODE == 'follow the wall step one':
-		right_side = min(Ranges[270:360])
-		left_side = min(Ranges[360:450])
+		right_side = min(Ranges[660:720])
+		left_side = min(Ranges[0:60])
 		print("left side:" + str(left_side))
 		print("right_side:" + str(right_side))
 		
@@ -192,58 +188,63 @@ def Follow_the_wall():
 			return
 			
 	if MODE == 'follow the wall step one by left':
-		if d_left > Obstacle_TH:
+		if d_left > Obstacle_TH or d_front < Obstacle_TH:
 			angz = -0.8
 			LINX = 0
 			return
 		else:
-			wall_distance = max(d_left, Obstacle_TH * 0.8)
+			wall_distance = d_left
 			MODE = 'follow the wall by left'
 			return
 	elif MODE == 'follow the wall step one by right' :
-		if d_right > Obstacle_TH:
+		if d_right > Obstacle_TH or d_front < Obstacle_TH:
 			angz = 0.8
 			LINX = 0
 			return
 		else:
-			wall_distance = max(d_right, Obstacle_TH * 0.8)
+			wall_distance = d_right
 			MODE = 'follow the wall by right'
 			return
 
 	theta = math.atan2(tgt_y - pos_y , tgt_x - pos_x)
 	delta = theta - yaw
 	
-	if delta > PI:
-		delta = delta - 2 * PI
-	elif delta < -PI:
+	if delta < 0:
 		delta = 2 * PI + delta
-	
-	index = int ((delta + PI) / angle_increment)
+	index = int (delta / angle_increment)
 	print("index:" + str (index))
-	if index < 45:
-		d_tgt = min(Ranges[index - 45 + 720: 720] + Ranges[0: index + 45])
-	elif index + 45 > 720:
-		d_tgt = min(Ranges[index - 45 : 720] + Ranges[0 : index + 45 - 720])
+	
+	if index < 60:
+		d_tgt = min(Ranges[index - 60 + 720: 720] + Ranges[0: index + 60])
+	elif index + 60 > 720:
+		d_tgt = min(Ranges[index - 60 : 720] + Ranges[0 : index + 60 - 720])
 	else:
-		d_tgt = min(Ranges[index - 45: index + 45])
+		d_tgt = min(Ranges[index - 60: index + 60])
 	
 	print("the distance in the target direction: " + str(d_tgt) )	
-	if d_tgt > Obstacle_TH * 2:
+	
+	
+	if d_tgt > Obstacle_TH * 3:
 		MODE = 'follow the line'
 		return
 	
-	if d_front < Obstacle_TH:
-		MODE = 'follow the wall step one'
-		return
 	else:
 		if MODE == 'follow the wall by right':
-			angz = Clip( (wall_distance - d_right) * 0.8, -PI * 0.4 , 0.4 * PI)
+			if d_front < Obstacle_TH:
+				MODE = 'follow the wall step one by right'
+				return
+			
+			angz = Clip( (wall_distance - d_right) * 0.6, -0.3 , 0.3)
 		elif MODE == 'follow the wall by left':
-			angz = Clip( -(wall_distance - d_left) * 0.8, -PI * 0.4 , 0.4 * PI)
+			if d_front < Obstacle_TH:
+				MODE = 'follow the wall step one by left'
+				return
+			
+			angz = Clip( -(wall_distance - d_left) * 0.6, -0.3 , 0.3)
+			
 		else:
 			print('some thing wrong!!!??')
-		LINX = Clip(0.4 * PI - abs(angz), 0.5, 1)  
-
+		LINX = 0.3
 		return
 
 def main():
@@ -255,12 +256,15 @@ def main():
 	global d_left
 	global tgt_x
 	global tgt_y
+	global wall_distance
 	
 	rospy.init_node('listener', anonymous=True)
 	
 	pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 	rospy.Subscriber("scan", sensor_msgs.msg.LaserScan, LaserScanProcess)
 	rospy.Subscriber("/odom", Odometry, Odometry_process)
+	
+	
 	rate = rospy.Rate(10)  # 10hz
 	
 	
@@ -269,19 +273,13 @@ def main():
 	while not rospy.is_shutdown():
 		print('the status:' + MODE)
 		
-
 		if MODE == 'follow the line':
 			Follow_the_line()
 		else:
 			Follow_the_wall()
 		
-		
-		if LINX == 0 and angz == 0:
-			LINX = -1
-			angz = (random.random() - 0.5)
-			
 		if stop:
-			LINZ = 0
+			LINX = 0
 			angz = 0
 		
 		command = Twist()
@@ -292,12 +290,11 @@ def main():
 		print("the distance in front of you: " + str(d_front))
 		print("the distance in right of you: " + str(d_right))
 		print("the distance in left of you: "  + str(d_left))
+		print('the wall distance:' + str(wall_distance))
 		print("the linx: " + str(LINX))
 		print("the angz: " + str(angz))
 		
-		
-		rate.sleep()
-		
+				
 		if not stop and Close(pos_x, pos_y, tgt_x, tgt_y):
 			if stage == 9:
 				stop = True
@@ -317,6 +314,8 @@ def main():
 					tgt_x = -10
 					tgt_y = 10
 					
+		rate.sleep()
+
 			
 
 
